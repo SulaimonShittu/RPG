600e5167aaff3aa9cf555d7bc09f2a262a02bfe1
sulee04 <71654123+sulee04@users.noreply.github.com>
2022-07-24 01:00:42 +0100

    updates.

---
+++ RPG/Controllers/CharacterController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RPG.Controllers;
+
+[ApiController]
+[Route("api/[Controller]/")]
+public class CharacterController : ControllerBase
+{
+    private static List<Character> characters = new List<Character>
+    {
+        new Character(),
+        new Character{Id = 1, Name = "Sam"}
+    };
+    
+    [HttpGet("GetAll")]
+    public ActionResult<List<Character>> Get()
+    {
+        return Ok(characters);
+    }
+                                                                            
+    [HttpGet]
+    public ActionResult<Character> GetSingle(int id)
+    {
+        return Ok(characters.FirstOrDefault(c => c.Id == id ));
+    }
+}
